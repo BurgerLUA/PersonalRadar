bc95ab232d56c9bbcf1b14eeeb7f21a3676156a9
BurgerLUA <mattwasnothere@gmail.com>
2015-12-06 18:13:09 -0800

    Ragdoll Support

--- lua/autorun/client/cl_personalradar.lua
+++ lua/autorun/client/cl_personalradar.lua
@@ -27,39 +27,49 @@ function PRDrawRadar()
 	
 	for k,v in pairs(player.GetAll()) do
 		if v ~= LocalPlayer() then
-		
-			local EnemyXYMod = v:GetPos() - Vector(0,0,v:GetPos().z)
-			local EnemyZMod = v:GetPos() - EnemyXYMod
-			local DistanceMod = LocalXYMod:Distance(EnemyXYMod)
-			local HeightMod = (EnemyZMod - LocalZMod).z
-
-			if DistanceMod <= (RadarSize*4.5) + 32 then
-			
-				local EnemyXYMod = v:GetPos() - Vector(0,0,v:GetPos().z)
-				local EnemyZMod = v:GetPos() - EnemyXYMod
-				local DistanceMod = LocalXYMod:Distance(EnemyXYMod)
-				local HeightMod = (EnemyZMod - LocalZMod).z
-			
-				if PRShouldShow(v) then
-					local FinalPos, FinalAng = WorldToLocal(EnemyXYMod,v:GetAngles(),LocalXYMod,LocalPlayer():GetAngles() + Angle(0,90,0))
-					FinalPos = FinalPos * 0.1
-					
-					local SelectedMat = TargetMat
-					
-					if v:Alive() == false then
-						SelectedMat = DeadTargetMat
-					end
-					
-					PRDrawOverlay(SelectedMat,COLOR_WHITE,RadarPosX - FinalPos.x,RadarPosY + FinalPos.y,math.Clamp(PlayerSize * (1 + HeightMod/500),PlayerSize / 2,PlayerSize*2),0)
-				end
-				
-			end
+			PRGenerateDot(v,LocalXYMod,LocalZMod,RadarPosX,RadarPosY)
 		end
+	end
+	
+	for k,v in pairs(ents.FindByClass("prop_ragdoll")) do
+		PRGenerateDot(v,LocalXYMod,LocalZMod,RadarPosX,RadarPosY)
 	end
 	
 end
 
 hook.Add("HUDPaint","PR: HUDPaint",PRDrawRadar)
+
+function PRGenerateDot(v,LocalXYMod,LocalZMod,RadarPosX,RadarPosY)
+
+	local EnemyXYMod = v:GetPos() - Vector(0,0,v:GetPos().z)
+	local EnemyZMod = v:GetPos() - EnemyXYMod
+	local DistanceMod = LocalXYMod:Distance(EnemyXYMod)
+	local HeightMod = (EnemyZMod - LocalZMod).z
+
+	if DistanceMod <= (RadarSize*4.5) + 32 then
+		local EnemyXYMod = v:GetPos() - Vector(0,0,v:GetPos().z)
+		local EnemyZMod = v:GetPos() - EnemyXYMod
+		local DistanceMod = LocalXYMod:Distance(EnemyXYMod)
+		local HeightMod = (EnemyZMod - LocalZMod).z
+	
+		if PRShouldShow(v) then
+			local FinalPos, FinalAng = WorldToLocal(EnemyXYMod,v:GetAngles(),LocalXYMod,LocalPlayer():GetAngles() + Angle(0,90,0))
+			FinalPos = FinalPos * 0.1
+			
+			local SelectedMat = TargetMat
+			
+			if v:IsPlayer() then
+				if v:Alive() == false then
+					SelectedMat = DeadTargetMat
+				end
+			end
+			
+			PRDrawOverlay(SelectedMat,COLOR_WHITE,RadarPosX - FinalPos.x,RadarPosY + FinalPos.y,math.Clamp(PlayerSize * (1 + HeightMod/500),PlayerSize / 2,PlayerSize*2),0)
+		end
+		
+	end
+end
+
 
 function PRShouldShow(ply)
 
@@ -72,7 +82,9 @@ function PRShouldShow(ply)
 		end
 	end
 
-	if (ply:GetVelocity():Length() > 110) or (ply:IsOnGround() == false) or (ply:Alive() == false) or (IsInSensor == true) then
+	if ply:IsPlayer() and (ply:Alive() == false) then
+		return true
+	elseif (ply:GetVelocity():Length() > 110) or (IsInSensor == true) then
 		return true
 	else
 		return false
